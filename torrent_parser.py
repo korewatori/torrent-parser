3955d4f608cc3a2f4ec874be74c2aae5376a8490
Tori <76889884+korewatori@users.noreply.github.com>
2022-12-22 00:14:03 +0000

    Update torrent_parser.py
    - Added the ability to display piece count + piece size in the "info" command*
    
    * this does not seem to be entirely accurate. qBittorrent reports a different number of pieces on one torrent compared to when I use that same torrent with this program.
    The piece size is entirely correct, though.
    To do: figure out what's wrong here.

--- torrent_parser.py
+++ torrent_parser.py
@@ -75,9 +75,10 @@ def display_torrent_info(torrent_file, output_file=None):
         name = "Unknown"
 
     human_date = "Unknown"  # Set default value for human_date
+    unix_timestamp_createdAt = 0  # Set default value for unix_timestamp_createdAt
     if b'creation date' in decoded_torrent:
         unix_timestamp_createdAt = decoded_torrent[b'creation date']
-        human_date = datetime.datetime.fromtimestamp(unix_timestamp_createdAt).strftime('%Y-%m-%d %H:%M:%S')
+        human_date = datetime.datetime.fromtimestamp(unix_timestamp_createdAt).strftime("%Y-%m-%d %H:%M:%S")
         
      # Extract the announce URLs
     if b'announce' in decoded_torrent:
@@ -103,11 +104,19 @@ def display_torrent_info(torrent_file, output_file=None):
         # If the 'files' field is not present, then the torrent file only contains a single file
         file_list = [{b'path': [decoded_torrent[b'info'][b'name']], b'length': decoded_torrent[b'info'][b'length']}]
 
-# Calculate the total size of all the files in the torrent
+    # Calculate the total size of all the files in the torrent
     total_size = 0
     for file in file_list:
         file_size = file[b'length']
         total_size += file_size
+
+   # Extract the number of pieces and piece size from the torrent
+    if b'pieces' in decoded_torrent[b'info']:
+        num_pieces = len(decoded_torrent[b'info'][b'pieces'])
+        piece_size = decoded_torrent[b'info'][b'piece length']
+    else:
+        num_pieces = "N/A"
+        piece_size = "N/A"
 
     # Get the file name of the torrent file
     torrent_file_name = os.path.basename(torrent_file)
@@ -120,11 +129,11 @@ def display_torrent_info(torrent_file, output_file=None):
     if output_file:
         output_file.write("- - - - - Details for {}: - - - - -\n\n".format(torrent_file_name))
         output_file.write("Name: {}\n".format(name))
-        output_file.write("Torrent creation date: {}\n".format(human_date))
-       # output_file.write("UNIX timestamp: {}\n".format(unix_timestamp_createdAt))
+        output_file.write("Torrent creation date: {} (Unix timestamp: {})\n".format(human_date, format(unix_timestamp_createdAt)))
         output_file.write("Number of files: {}\n".format(len(file_list)))
         output_file.write("Total size: {}\n".format(format_size(total_size)))
         output_file.write("Torrent infohash: {}\n".format(info_hash))
+        output_file.write("Number of Pieces: {} (x {})\n".format(num_pieces, format_size(piece_size)))
         output_file.write("\nAnnounce URL(s):\n")
         for url in announce_urls:
                 output_file.write("{}\n".format(url))
@@ -132,11 +141,11 @@ def display_torrent_info(torrent_file, output_file=None):
     else:
         print("\n- - - - - Details for {}: - - - - -\n".format(torrent_file_name))
         print("Name: {}".format(name))
-        print("Torrent creation date: {}".format(human_date))
-      #  print("UNIX timestamp: {}".format(unix_timestamp_createdAt))
+        print("Torrent creation date: {} (Unix timestamp: {})".format(human_date, format(unix_timestamp_createdAt)))
         print("Number of files: {}".format(len(file_list)))
         print("Total size: {}".format(format_size(total_size)))
-        print("Torrent infohash: {}".format(info_hash))        
+        print("Torrent infohash: {}".format(info_hash))
+        print("Number of Pieces: {} (x {})".format(num_pieces, format_size(piece_size)))       
         print("\nAnnounce URL(s):")
         for url in announce_urls:
                 print("{}\n".format(url))
